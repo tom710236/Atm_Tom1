55505d8737f740dd9edf2fc681ee8408c4a973d8
TOM <tom710236@gmail.com>
2017-02-10 10:52:23 +0800

    設定選單功能

--- app/src/main/java/com/example/tom/atm_tom1/MainActivity.java
+++ app/src/main/java/com/example/tom/atm_tom1/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -70,9 +72,28 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     //取消鍵
     public void onClick2(View v){
         finish();
     }
+
+
+
 
 }
